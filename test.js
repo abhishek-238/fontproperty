d41dbe7f9d965de947fac349f23abafa261c3312
Abhishek dubey <abhishekraj1jnp@gmail.com>
2021-08-25 13:59:23 +0530

    second repository

---
+++ test.js
@@ -0,0 +1,27 @@
+function fun1(){
+    document.querySelector("p").style.color = "green";
+}
+function fun2(){
+    document.querySelector("p").style.color = "blue";
+}
+function fun3(){
+    document.querySelector("p").style.color = "red";
+}
+function fun4(){
+    document.querySelector("p").style.fontSize = "12px";
+}
+function fun5(){
+    document.querySelector("p").style.fontSize = "15px";
+}
+function fun6(){
+    document.querySelector("p").style.fontSize = "20px";
+}
+function fun7(){
+    document.querySelector("p").style.fontFamily = "sans-serif";
+}
+function fun8(){
+    document.querySelector("p").style.fontFamily = 'Courier New', Courier, monospace;
+}
+function fun9(){
+    document.querySelector("p").style.fontFamily = "monospace";
+}
